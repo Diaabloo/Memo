4e28cb2882124d09baae389925b51d0cbfa53eb0
Amine El Alami <amineelalami05@gmail.com>
2023-05-09 13:49:08 +0100

    Memo

---
+++ src/components/button.vue
@@ -0,0 +1,19 @@
+<template>
+      <button @click="onclick()" :style="{background: color}" class="btn">{{ text }}</button>
+</template>
+
+<script>
+
+export default{
+      name : 'ButTon',
+      props:{
+            text : String,
+            color : String,
+      },  
+      methods:{
+            onclick(){
+                  this.$emit('btn-click')
+            }
+      }
+}
+</script>
---
+++ src/components/header.vue
@@ -0,0 +1,41 @@
+<template>
+      <header>
+            <h1 class="title">{{title}}</h1>
+            <ButTon @btn-click="$emit('toggle-add-task')" 
+                    :text="showAddTask ? 'Close' : 'Add Task'" 
+                    :color="showAddTask ? 'red' : 'green'"/>
+      
+      <!--
+      For Add More Button from component 'Button':
+
+      <ButTon text="Upd Task" color="black"/>
+      <ButTon text="Del Task" color="red"/> 
+      -->
+      </header>
+
+</template>
+
+<script>
+      import ButTon from './button.vue'
+      export default{
+            // eslint-disable-next-line vue/multi-word-component-names
+            name: 'Header',
+            props:{
+                  title: String,
+                  showAddTask: Boolean
+            },
+      components : {
+            ButTon
+      }
+      }
+</script> 
+
+<style scoped>
+      header{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 20px;
+      }
+
+</style>
